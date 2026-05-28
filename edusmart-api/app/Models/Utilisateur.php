b08df9d5638a56dedd22b8edd3cfabe214e48ef4
Corei913th <bogningfredy972@gmail.com>
2026-05-28 22:39:17 +0100

    refactor: extract relation methods into HasRelations trait to respect Sonar 20-method threshold

--- edusmart-api/app/Models/Utilisateur.php
+++ edusmart-api/app/Models/Utilisateur.php
@@ -4,14 +4,12 @@ namespace App\Models;
 
 use App\Constants\TokenConstants;
 use App\Enums\Role;
+use App\Models\Traits\HasRelations;
 use App\Traits\HasEncryptedPii;
 use App\Traits\HasUpdatedAtTrigger;
 use App\Traits\UsesUuidAsPrimaryKey;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
@@ -33,6 +31,7 @@ class Utilisateur extends Authenticatable {
     use HasApiTokens;
     use HasEncryptedPii;
     use HasFactory;
+    use HasRelations;
     use HasUpdatedAtTrigger;
     use UsesUuidAsPrimaryKey;
     public const ROLE_DEFAULT = 'ENSEIGNANT';
@@ -72,77 +71,5 @@ class Utilisateur extends Authenticatable {
 
     public function markAsConnected(): void {
         $this->update(['derniere_connexion' => now()]);
-    }
-
-    public function etablissement(): BelongsTo {
-        return $this->belongsTo(Etablissement::class);
-    }
-
-    public function enseignant(): HasOne {
-        return $this->hasOne(Enseignant::class, 'utilisateur_id');
-    }
-
-    public function personnelAdministratif(): HasOne {
-        return $this->hasOne(PersonnelAdministratif::class, 'utilisateur_id');
-    }
-
-    public function eleve(): HasOne {
-        return $this->hasOne(Eleve::class, 'utilisateur_id');
-    }
-
-    public function parentTuteur(): HasOne {
-        return $this->hasOne(ParentTuteur::class, 'utilisateur_id');
-    }
-
-    public function sessions(): HasMany {
-        return $this->hasMany(Session::class);
-    }
-
-    public function otpCodes(): HasMany {
-        return $this->hasMany(OtpCode::class);
-    }
-
-    public function auditLogs(): HasMany {
-        return $this->hasMany(AuditLog::class);
-    }
-
-    public function pushSubscriptions(): HasMany {
-        return $this->hasMany(PushSubscription::class);
-    }
-
-    public function syncQueues(): HasMany {
-        return $this->hasMany(SyncQueue::class);
-    }
-
-    public function messagesEnvoyes(): HasMany {
-        return $this->hasMany(Message::class, 'expediteur_id');
-    }
-
-    public function notifications(): HasMany {
-        return $this->hasMany(Notification::class, 'destinataire_id');
-    }
-
-    public function notesCrees(): HasMany {
-        return $this->hasMany(Note::class, 'created_by');
-    }
-
-    public function absencesCrees(): HasMany {
-        return $this->hasMany(Absence::class, 'created_by');
-    }
-
-    public function appreciationsCrees(): HasMany {
-        return $this->hasMany(AppreciationComportementale::class, 'created_by');
-    }
-
-    public function transfertsCrees(): HasMany {
-        return $this->hasMany(Transfert::class, 'created_by');
-    }
-
-    public function radiationsCrees(): HasMany {
-        return $this->hasMany(Radiation::class, 'created_by');
-    }
-
-    public function participantsFil(): HasMany {
-        return $this->hasMany(ParticipantFil::class, 'utilisateur_id');
     }
 }
